1ca080adb528b1c9127b47f9258d36e9e1a0d164
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-15 23:28:28 +0530

    added file of  UC3_PART_TIME_EMPLOYEE

--- Program.cs
+++ Program.cs
@@ -10,8 +10,42 @@ namespace employeeewagee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+            // Constant Variable
+            int FullTime = 1;                                        //creating a local variable
+            int PartTime = 2;
+            int empRatePerHour = 20;
+
+            //Local Variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();                           //Generating Random value
+            int employeeCheck = random.Next(0, 3);                  // assigning Random value to the variable 
+            Console.WriteLine("Random Value: " + employeeCheck);
+
+
+            if (employeeCheck == FullTime)                          //Checking whether Employee is Present or Not
+            {
+                Console.WriteLine("Employee is PRESENT");
+                empHrs = 8;
+            }
+            else if (employeeCheck == PartTime)
+            {
+                Console.WriteLine("Part Time Empoyee: ");
+                empHrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employess is ABSENT");
+                empHrs = 0;
+            }
+
+
+            // computation
+            empWage = empRatePerHour * empHrs;
+            Console.WriteLine("Employee wage per day: " + empWage);
+            Console.Read();
         }
     }
 }
+        
