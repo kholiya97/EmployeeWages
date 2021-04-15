f39362fd3a6ba151a8b2bdbe2156929df8bcedaa
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 00:41:18 +0530

    Added file of UC6_CALC_EMPWAGE_FOR_100HRS

--- Program.cs
+++ Program.cs
@@ -8,10 +8,53 @@ namespace employeeewagee
 {
     class Program
     {
+        const int FullTime = 1;                                        //creating a local variable
+        const int PartTime = 2;
+        public const int maxWorkingDays = 20;
+        public const int empRatePerHour = 20;
+        public const int maxWorkingHours = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+           // Console.WriteLine("Welcome to Employee wage Calculation....");
+
+            //Local Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int workingDays = 1;
+            int totalEmpWage = 0;
+            int totalHrs = 0;
+
+            Random random = new Random();                           //Generating Random value
+            while (totalHrs < maxWorkingHours && workingDays < maxWorkingDays)
+            {
+                workingDays++;
+                int employeeCheck = random.Next(0, 3);                      // assigning Random value to the variable 
+                                                                            // Console.WriteLine("Random Value: " + employeeCheck);
+
+                //USING SWITCH STATEMENT
+                switch (employeeCheck)
+                {
+                    case FullTime:
+                        empHrs = 8;
+                        break;
+
+                    case PartTime:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empRatePerHour * empHrs;
+                totalHrs = totalHrs + empHrs; //calculting emp hours
+                totalEmpWage += empWage; //calculating emp total wage
+            } //END of WHILE LOOP
+
+            Console.WriteLine("Employee wage for" + workingDays + " day: " + totalEmpWage);
+            Console.WriteLine("Working Hours: " + totalHrs);
+            Console.Read();
         }
     }
 }
+   
