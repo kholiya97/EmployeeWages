fcf58eff2ac07d7f198e49b5cb11a43163457941
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 00:24:01 +0530

    added file of UC4_EMPWAGE_USING_CASE_STATEMENT

--- Program.cs
+++ Program.cs
@@ -8,10 +8,43 @@ namespace employeeewagee
 {
     class Program
     {
+        const int FullTime = 1;                                        //creating a local variable
+        const int PartTime = 2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+            // Constant Variable
+
+            int empRatePerHour = 20;
+
+            //Local Variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();                           //Generating Random value
+            int employeeCheck = random.Next(0, 3);                  // assigning Random value to the variable 
+            Console.WriteLine("Random Value: " + employeeCheck);
+
+            //using Case statement 
+            switch (employeeCheck)
+            {
+                case FullTime:
+                    empHrs = empHrs + 8;
+                    break;
+
+                case PartTime:
+                    empHrs = empHrs + 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+
+            // computation
+            empWage = empRatePerHour * empHrs;
+            Console.WriteLine("Employee wage per day: " + empWage);
+            Console.Read();
         }
     }
 }
