600a1ed0d2924c438054c7c74014f14ad99c70aa
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-15 23:02:27 +0530

    Added file of UC1-EMP_PRESENT_ABSENT

--- Program.cs
+++ Program.cs
@@ -10,8 +10,21 @@ namespace employeeewagee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+            int Present = 1;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+            Console.WriteLine("EmpCheck: " + EmpCheck);
+
+            if (EmpCheck == Present)
+            {
+                Console.WriteLine("Employee PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("Employee ABSENT");
+            }
+            Console.Read();
         }
     }
 }
+
