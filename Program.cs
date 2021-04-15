475b03f1f24f8d3a4b4d5e22d3e3c393feb7dfa7
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-15 23:20:44 +0530

    Added file of UC2_CALC_DAILY_EMP_WAGE

--- Program.cs
+++ Program.cs
@@ -10,8 +10,34 @@ namespace employeeewagee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+            // Constant Variable
+            int FullTime = 1;                                        //creating a local variable
+            int empRatePerHour = 20;
+
+            //Local Variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();                           //Generating Random value
+            int employeeCheck = random.Next(0, 2);       // assigning Random value to the variable 
+            Console.WriteLine("Random Value: " + employeeCheck);
+
+            if (employeeCheck == FullTime)                 //Checking whether Employee is Present or Not
+            {
+                Console.WriteLine("Employee is PRESENT");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employess is ABSENT");
+                empHrs = 0;
+            }
+
+            // computation
+            empWage = empRatePerHour * empHrs;
+            Console.WriteLine("Employee wage per day: " + empWage);
+            Console.Read();
         }
     }
 }
+       
