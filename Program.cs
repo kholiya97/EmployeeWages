50bdf9794af7cd06415ea80713896904f416ddf5
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 01:21:29 +0530

    Added file of UC8_CALC_EMPWAGE_FOR_MULTIPLE_COMPANIES

--- Program.cs
+++ Program.cs
@@ -8,10 +8,67 @@ namespace employeeewagee
 {
     class Program
     {
+        
+        /// <summary>
+        /// Creating Multiple Company
+        /// </summary>
+        //constants
+        public const int Full_TIME = 1;
+        public const int PART_TIME = 2;
+        //public const int EMP_RATE_PER_HR = 20;
+        //public const int MAX_WORKING_DAYS = 20;
+        //public const int MAX_WORKING_HRS = 100;
+
+        public static void ComputeWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
+        {
+            //local variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            int hrs = 0;
+            int WorkingDays = 1;
+
+
+            //inbuilt class
+            Random random = new Random();
+            while (hrs < maxWorkingHours && WorkingDays <= maxWorkingDays)
+            {
+                WorkingDays++;
+                int employeeCheck = random.Next(0, 3);
+
+                //Console.WriteLine("random value " + employeeCheck);
+
+                //selection statements
+
+                switch (employeeCheck)
+                {
+                    case Full_TIME:
+                        empHrs = 8;
+                        break;
+
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                //computation
+                empWage = wagePerHour * empHrs;
+                hrs = empHrs;
+                totalEmpWage += empWage;//totalEmpWage=totalWage+empWage
+            }
+            Console.WriteLine("Emp wage for " + companyName + "\t is\t" + totalEmpWage);
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewages");
-                Console.ReadLine();
+            ComputeWage("Demart", 20, 20, 40);
+            ComputeWage("Reliance", 15, 15, 30);
+            Console.Read();
+
         }
     }
 }
+   
